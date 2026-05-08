77cb427b01f5f4a10f98bb197a451bec9b09744d
Victor Durodola <victordurodola@Victors-Mac-mini.local>
2026-05-08 15:09:07 -0500

    ..

--- src/Components/nav-bar.jsx
+++ src/Components/nav-bar.jsx
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <div className='nav-container'>
     
-        <img src="pursuetruth/src/assets/WHITE-LOGO.svg" alt="logo" width={90} height={90}/>
+        <img src="src/assets/WHITE-LOGO.svg" alt="logo" width={90} height={90}/>
 
         <div className='menu'>
                 <a href="https://thechurch.shop/shop/pursuetruth/">
